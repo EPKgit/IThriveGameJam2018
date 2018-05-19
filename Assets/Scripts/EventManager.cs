a2ba718e33e04bb46d3a660d262c0effb4556059
Christian Gramling <gramlinc@uci.edu>
2018-05-19 15:23:54 -0700

    Bugged Fixes and then Fixed Bugs

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,13 @@ public class EventManager : Singleton<EventManager>
 	
 	public float gameTime;
 
-	public Event[] events;
+	public List<Event> events;
 	private List<Event> activeEvents;
 	//public Queue<Event> events;
 
 	void Start () 
 	{
+		events = new List<Event>();
 		activeEvents = new List<Event>();
 		gameTime = 0;
 		StartCoroutine(RunGame());
@@ -20,21 +22,22 @@ public class EventManager : Singleton<EventManager>
 	
 	IEnumerator RunGame()
 	{
+		yield return new WaitUntil( () => BackgroundManager.instance != null);
 		while(true)
 		{	
 			gameTime += Time.deltaTime;
-			/*
+			
 			foreach (Event e in events)
 			{
-				if (!e.checkTriggers(gameTime)) // tries to start events. True means it's a one time event, so don't add to activeEvents
+				if (!e.checkTrigger(gameTime)) // tries to start events. True means it's a one time event, so don't add to activeEvents
 					activeEvents.Add(e);
 			}
 			foreach (Event e in activeEvents)
 			{
-				if (e.checkOutcomes(gameTime)) // resolves outcomes for event. True means the outcome terminates the event.
+				if (e.checkOutcome(gameTime)) // resolves outcomes for event. True means the outcome terminates the event.
 					activeEvents.Remove(e);
 			}
-			*/
+			
 			BackgroundManager.instance.MoveBackground(Time.deltaTime);
 			yield return null;
 		}
