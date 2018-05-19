d97d765a6607ab81fe41c2b38f1ed1b0340cd1c6
Eric Kim <ericpaulkim@gmail.com>
2018-05-19 10:51:20 -0700

    Organization

---
+++ Assets/EventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour 
+{
+	public Event[] events;
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Events/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour 
+{
+	public Character[] characters;
+	public bool[] triggers;
+	public Outcome[] outcomes;
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
