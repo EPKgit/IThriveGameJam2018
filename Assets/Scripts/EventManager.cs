1df6c9100d599e33078bcb3d46d8368ecb46b774
Christian Gramling <gramlinc@uci.edu>
2018-05-19 13:52:26 -0700

    Created Event System

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -7,10 +7,12 @@ public class EventManager : Singleton<EventManager>
 	public float gameTime;
 
 	public Event[] events;
+	private List<Event> activeEvents;
 	//public Queue<Event> events;
 
 	void Start () 
 	{
+		activeEvents = new List<Event>();
 		gameTime = 0;
 		StartCoroutine(RunGame());
 	}
@@ -18,6 +20,18 @@ public class EventManager : Singleton<EventManager>
 	IEnumerator RunGame()
 	{
 		gameTime += Time.deltaTime;
+		
+		foreach (Event e in events)
+		{
+			if (!e.checkTriggers(gameTime)) // tries to start events. True means it's a one time event, so don't add to activeEvents
+				activeEvents.Add(e);
+		}
+		foreach (Event e in activeEvents)
+		{
+			if (e.checkOutcomes(gameTime)) // resolves outcomes for event. True means the outcome terminates the event.
+				activeEvents.Remove(e);
+		}
+
 		yield return null;
 	}
 
--- Assets/Scripts/Events/Event.cs
+++ Assets/Scripts/Events/Event.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Event : MonoBehaviour 
+public class Event : Outcome
 {
-	public Character[] characters;
-	public bool[] triggers;
-	public Outcome[] outcomes;
-	void Start () 
-	{
-		
-	}
 	
-	void Update () 
+	public Outcome[] outcomes; 
+	
+
+	Event(Func<>[] a, Func<bool>[] t, Outcome[] o) : base(a, t)
 	{
-		
+		outcomes = o; // it acts like an outcome- is triggered, causes things, can end
+						// except it has a list of outcomes it iterates through like the event manager =
+	}
+
+	public bool checkOutcomes(float time) 
+	{
+		// similar to event manager... keeps checking outcomes until one of them causes an end
+		foreach (Outcome o in outcomes)
+		{
+			if (o.checkTriggers(time - callTime))
+				return true; // ends the event
+		}
+		return false;
 	}
 }
