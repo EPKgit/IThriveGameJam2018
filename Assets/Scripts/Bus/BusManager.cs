cdb5bb65cbd11978a7631cb7a6c8124f2316349f
Eric Kim <ericpaulkim@gmail.com>
2018-05-19 11:06:58 -0700

    Bus Manager Skeleton

--- Assets/Scripts/Bus/BusManager.cs
+++ Assets/Scripts/Bus/BusManager.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BusManager : MonoBehaviour {
+public class BusManager : Singleton<BusManager>
+{
+	public GameObject[] gseats;
 
-	// Use this for initialization
-	void Start () {
-		
+	private Seat[] seats;
+	private int size;
+
+	void Start () 
+	{
+		base.EnforceSingleton();
+		size = gseats.Length;
+		seats = new Seat[size];
+		for(int x = 0; x < size; ++x)
+			if( (seats[x] = gseats[x].GetComponent<Seat>()) == null )
+				Debug.Log("FATAL ERROR: SEAT NOT ATTACHED TO GAMEOBJECT" + gseats[x].name);
+	}
+
+	Vector2 seatLocation(int index)
+	{
+		if(index < 0 || index >= size)
+			return new Vector2(0,0);
+		return gseats[index].transform.position;
+	}
+
+	bool seatOccupied(int index)
+	{
+		if(index >= 0 && index < size)
+			return seats[index].occupied;
+		return false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 }
