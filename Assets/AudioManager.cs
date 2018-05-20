e5c6748f2d469b12b5599fcd3d7548781067825d
Eric Kim <ericpaulkim@gmail.com>
2018-05-20 12:43:48 -0700

    main audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager> 
+{
+	private float mvolume;
+	public float s_volume;
+
+	public float m_volume
+	{
+		get
+		{
+			return mvolume;
+		}
+
+		set
+		{
+			mvolume = value;
+		}
+	}
+
+	void Start () 
+	{
+		mvolume = 1;
+		s_volume = 1;	
+	}
+	
+}
---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour 
+{
+	public AudioSource mainMenuMusic;
+
+	void Start () 
+	{
+		mainMenuMusic.Play();
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	public void LoadGame()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void SwitchToSettings()
+	{
+
+	}
+}
