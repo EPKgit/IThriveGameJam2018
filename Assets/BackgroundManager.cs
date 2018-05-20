5b2c3db8f6e1ead254695cc65dc443a592b9a0a0
Eric Kim <ericpaulkim@gmail.com>
2018-05-19 23:46:53 -0700

    Revert "beauty and grace parallax my face in please"
    This reverts commit 85651def7ab4b98e6605a55b35e5a479c37e63e7.

--- Assets/BackgroundManager.cs
+++ Assets/BackgroundManager.cs
@@ -21,48 +21,22 @@ public class BackgroundManager : Singleton<BackgroundManager>
 
 	public LayerMask layer;
 
-	private float busStartingY;
-	private float fWStartingY;
-	private float bWStartingY;
-	
-
 	void Start () 
 	{
 		base.EnforceSingleton();
 		Physics2D.IgnoreLayerCollision(layer, layer, true);
-		busStartingY = bus.transform.position.y;
-		fWStartingY = fWheel.transform.position.y;
-		bWStartingY = bWheel.transform.position.y;
 	}
 	
-	void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.F))
-			Time.timeScale = 20;
-		if(Input.GetKeyUp(KeyCode.F))
-			Time.timeScale = 1;
-	}
-
 	public void MoveBackground(float time)
 	{
-		Vector3 pos = bus.transform.position;
-		bus.transform.position = new Vector3(pos.x, busStartingY + (Time.deltaTime * Mathf.Sin(busBouncePeriod * time) * busBounceAmount), pos.z );//(Mathf.Sin(time) < 0 ? Vector2.up : Vector2.down) * busBounce);
+		bus.transform.Translate(Time.deltaTime * Mathf.Sin(busBouncePeriod * time) * Vector3.up * (1/busBounceAmount) );//(Mathf.Sin(time) < 0 ? Vector2.up : Vector2.down) * busBounce);
 		bWheel.transform.Rotate(0,0,-3);
+		bWheel.transform.Translate(Time.deltaTime * Mathf.Sin(busBouncePeriod * time - 1) * Vector3.up * (1/busBounceAmount) );//(Mathf.Sin(time) < 0 ? Vector2.up : Vector2.down) * busBounce);
 		fWheel.transform.Rotate(0,0,-3);
-		pos = bWheel.transform.position;
-		bWheel.transform.position = new Vector3(pos.x, fWStartingY + (Time.deltaTime * Mathf.Sin(busBouncePeriod * time - 1) * busBounceAmount), pos.z );//(Mathf.Sin(time) < 0 ? Vector2.up : Vector2.down) * busBounce);
-		pos = fWheel.transform.position;
-		fWheel.transform.position = new Vector3(pos.x, bWStartingY + (Time.deltaTime * Mathf.Sin(busBouncePeriod * time - 1) * busBounceAmount), pos.z );//(Mathf.Sin(time) < 0 ? Vector2.up : Vector2.down) * busBounce);
-		
+		fWheel.transform.Translate(Time.deltaTime * Mathf.Sin(busBouncePeriod * time - 1) * Vector3.up * (1/busBounceAmount) );//(Mathf.Sin(time) < 0 ? Vector2.up : Vector2.down) * busBounce);
 		bg0.transform.Translate(Time.deltaTime * Vector3.left * speed);
 		bg1.transform.Translate(Time.deltaTime * Vector3.left * speed * bg1PercentIncrease);
-		if(bg1.transform.position.x + 1000 < bus.transform.position.x)
-			Debug.Log("1");
 		bg2.transform.Translate(Time.deltaTime * Vector3.left * speed * bg1PercentIncrease * bg2PercentIncrease);
-		if(bg2.transform.position.x + 1000 < bus.transform.position.x)
-			Debug.Log("2");
 		bg3.transform.Translate(Time.deltaTime * Vector3.left * speed * bg1PercentIncrease * bg2PercentIncrease * bg3PercentIncrease);
-		if(bg3.transform.position.x + 1000 < bus.transform.position.x)
-			Debug.Log("3");
 	}
 }
